23f414fa56a6567e80cb37024b6d4be9c6b76716
Aboudourazakou <tetereou2017@gmail.com>
2021-10-10 22:25:36 +0100

    accueil

---
+++ components/MainHeader.vue
@@ -0,0 +1,201 @@
+<template>
+  <!-- App.vue -->
+
+  <!--App-bar-->
+  <v-container fluid class="header-container">
+    <v-card class="header-card" height="72" flat>
+      <!--menu container-->
+      <v-container class="d-flex mx-auto">
+        <!--Logo Dunia-->
+        <div class="logo-wrapper d-flex mr-4" style="width:222px;padding-left:0px;">
+          <div class="logo">
+            <v-img src="/dunia.jpeg" />
+          </div>
+        </div>
+        <!--End logo Dunia-->
+        <!--Search bar-->
+        <div class="find d-flex">
+          <div class="search">
+            <v-img src="/shop/search.svg"></v-img>
+          </div>
+          <input
+            class="input"
+            type="text"
+            placeholder="Chercher un produit,une marque,ou une categorie"
+          />
+        </div>
+        <!-- End search bar-->
+        <!--Search button-->
+        <div>
+          <v-btn class="btn-search ml-2" elevation="5" height="40">Rechercher</v-btn>
+        </div>
+        <!--End search button-->
+
+        <!--logged in user icon and indications-->
+        <div class="user d-flex ml-3" style="align-items:center; text-transform: capitalize;">
+          <v-img src="/shop/user.svg"></v-img>
+          <h3 class="username d-inline-block text-truncate">Bonjour, Aboudourazakou</h3>
+          <v-img height="5px" src="/shop/dropdown.svg"></v-img>
+        </div>
+        <div class="help d-flex ml-3">
+          <v-img src="/shop/help.svg" class="mr-2"></v-img>
+          <h3>Aide</h3>
+          <v-img src="/shop/dropdown.svg"></v-img>
+        </div>
+        <div class="basket d-flex mt-2">
+          <v-badge color="#f68b1e" content="5" overlap>
+            <v-img src="/shop/basket.svg"></v-img>
+          </v-badge>
+          <h3 class="ml-3">Panier</h3>
+        </div>
+        <!--end logged in icon and indications -->
+      </v-container>
+      <!--End menu container-->
+    </v-card>
+  </v-container>
+  <!--End App-bar-->
+</template>
+
+
+
+
+<style scoped>
+/*a*/
+a {
+  list-style: none;
+  text-decoration: none;
+  color: black;
+}
+/*b*/
+.basket > h3 {
+  font-size: 16px;
+  font-weight: 500;
+  color: #000000;
+}
+.btn-search {
+  background: #e4811c !important;
+  color: #ffffff;
+  font-size: 14px;
+}
+/*c*/
+
+/*d*/
+/*e*/
+/*f*/
+.find {
+  align-items: center;
+  border-radius: 5px;
+  border: 1px solid #c7c7cd;
+  background: rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box
+    border-box;
+  background-attachment: scroll;
+  background-image: none;
+  background-position: 0% 0%;
+  background-repeat: repeat;
+  color: #282828;
+  font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Helvetica Neue", Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  height: 40px;
+  line-height: normal;
+  margin: 0px;
+  padding: 0px;
+  width: 350px;
+}
+/*g*/
+/*h*/
+.header-card {
+  background: #ffffff;
+}
+.header-container {
+  padding: 0;
+}
+.help {
+  font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  height: 40px;
+  line-height: normal;
+  margin: 0px;
+  padding: 10px;
+  width: 107px;
+}
+.help > h3 {
+  font-size: 16px;
+  font-weight: 500;
+  color: #000000;
+}
+/*i*/
+.input {
+  border: none;
+  background: rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box
+    border-box;
+  background-attachment: scroll;
+  background-color: #ffffff;
+  background-image: none;
+  background-position: 0% 0%;
+  background-repeat: repeat;
+  color: #000000;
+  font-family: Arial;
+  font-size: 16px;
+  font-weight: 400;
+  height: 38px;
+  line-height: normal;
+  margin: 0px 8px 0px 0px;
+  padding: 1px 2px;
+  width: 399px;
+}
+input:focus {
+  outline: none !important;
+}
+/*j*/
+/*k*/
+/*l*/
+.logo {
+  width: 180px;
+}
+
+/*m*/
+/*n*/
+/*o*/
+/*p*/
+::placeholder {
+  text-overflow: ellipsis;
+}
+[placeholder] {
+  text-overflow: ellipsis;
+}
+/*q*/
+/*r*/
+/*s*/
+.search {
+  margin: 0px 8px;
+  align-items: center;
+}
+/*t*/
+/*u*/
+.username {
+  background: rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box
+    border-box;
+  background-attachment: scroll;
+  background-repeat: repeat;
+  font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  height: 40px;
+  line-height: normal;
+  padding: 10px;
+  width: 183px;
+  color: #000000;
+}
+.username:hover {
+  color: #f68b1e;
+}
+/*v*/
+/*w*/
+/*x*/
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,282 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+  <v-app>
+    <MainHeader />
+    <v-main>
+      <v-container class="banner d-flex mx-auto mt-3">
+        <!--Menu on big devices not include mobile-->
+        <v-card flat width="222" class="mr-3">
+          <v-list style="background:#ffffff">
+            <span v-for="item in items" :key="item.title" class="d-flex" style="padding-left:8px">
+              <v-list-item-content class="menu-item-content" :id="item.icon.split('.')[0]">
+                <v-list-item-title class="menu-item">
+                  <v-avatar size="16" class="mr-1">
+                    <v-img :src="'/iconsmenu/' + item.icon"></v-img>
+                  </v-avatar>
+                  {{ item.title }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </span>
+          </v-list>
+        </v-card>
+
+        <!--End menu-->
+
+        <!--Caroussel-->
+        <v-card flat height="384" width="712">
+          <!-- carroussel Buttons-->
+          <div class="arrows d-flex">
+            <a v-for="n in 5" :key="n">
+              <v-icon
+                v-on:click="changer(n)"
+                class="icon"
+                :class="{ active: affiche == n }"
+              >mdi-checkbox-blank-circle-outline</v-icon>
+            </a>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+          <!--End caroussel buttons-->
+          <transition name="component-fade" mode="out-in">
+            <v-img class="c-image" v-show="affiche == 1" src="/5.png"></v-img>
+          </transition>
+
+          <transition name="component-fade" mode="out-in">
+            <v-img class="c-image" v-show="affiche == 2" src="/2.jpg"></v-img>
+          </transition>
+
+          <transition name="component-fade" mode="out-in">
+            <v-img class="c-image" v-show="affiche == 3" src="/3.jpg"></v-img>
+          </transition>
+
+          <transition name="component-fade" mode="out-in">
+            <v-img class="c-image" v-show="affiche == 4" src="/6.jpg"></v-img>
+          </transition>
+
+          <transition name="component-fade" mode="out-in">
+            <v-img class="c-image" v-show="affiche == 5" src="/7.jpg"></v-img>
+          </transition>
+        </v-card>
+        <!--End Caroussel-->
+
+        <!--Right column-->
+        <div class="d-block ml-3">
+          <v-card width="218" height="auto" style="border-radius:5px" class="mb-4">
+            <div
+              class="list-wrapper d-flex pt-4 pl-2"
+              style="padding-bottom:14.5px"
+              v-for="info in infos"
+              :key="info.title"
+            >
+              <v-avatar size="30" class="mr-3">
+                <v-img :src="'/' + info.image"></v-img>
+              </v-avatar>
+              <div class="list-content d-block">
+                <h4 class="pb-0 m-0 infotitle">{{ info.title }}</h4>
+                <h5 class="pt-0 mt-0 infosubtitle">{{ info.info }}</h5>
+              </div>
+            </div>
+          </v-card>
+          <v-img style="border-radius:5px" src="/4.jpg" width="auto" height="auto"></v-img>
+        </div>
+        <!--End Right column-->
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      affiche: 1,
+      infos: [
+        {
+          title: "Centre d'assistance",
+          info: "Guide du service client",
+          image: "interrogation.png"
+        },
+        {
+          title: "Retour facile",
+          info: "Remboursement rapide",
+          image: "retourfacile.png"
+        },
+        {
+          title: "Creer votre Dunia",
+          info: "Ouvrez votre shop ici",
+          image: "createshop.png"
+        }
+      ],
+      items: [
+        {
+          icon: "beauty.svg",
+          title: "Beauté && Santé",
+          to: "/Medecin"
+        },
+        {
+          icon: "clothes.svg ",
+          title: "Vêtements et Chaussures",
+          to: "/pharmacies"
+        },
+        {
+          icon: "computer.svg",
+          title: "Accessoires et Mode",
+          to: "/restaurants"
+        },
+
+        {
+          icon: "house.svg",
+          title: "Maison & Cuisine",
+          to: "/boutiques"
+        },
+
+        {
+          icon: "phone.svg",
+          title: "Téléphone & Tablette",
+          to: "/traducteurs"
+        },
+        {
+          icon: "washing.svg",
+          title: "Electroménager",
+          to: "/traducteurs"
+        },
+        {
+          icon: "computer.svg",
+          title: "Informatique",
+          to: "/traducteurs"
+        },
+
+        {
+          icon: "supermarket.svg",
+          title: "Supermarché",
+          to: "/traducteurs"
+        },
+        {
+          icon: "sport.svg",
+          title: "Sports & Loisirs ) ",
+          to: "/traducteurs"
+        },
+        {
+          icon: "television.svg",
+          title: "TV & High Tech ",
+          to: "/traducteurs"
+        },
+        {
+          icon: "baby.svg",
+          title: "Bébé & Jouets",
+          to: "/traducteurs"
+        },
+        {
+          icon: "other.svg",
+          title: "Autres catégories",
+          to: "/traducteurs"
+        }
+      ]
+    };
+  },
+  methods: {
+    changer(n) {
+      this.affiche = n;
+    },
+    carroussel() {
+      this.affiche = this.affiche == 5 ? 1 : this.affiche + 1;
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.carroussel();
+    }, 5000);
+    setInterval(() => {
+      this.dateRemain = this.expireIn - Math.round(Date.now() / 1000);
+    }, 1000);
+  }
+};
+</script>
+
+
+<style scoped>
+/*a*/
+.active {
+  background: #f68b1e !important;
+  border-radius: 100%;
+  color: #f68b1e !important;
+}
+.arrows {
+  position: absolute;
+  z-index: 10;
+  left: 45%;
+  bottom: 15px;
+}
+/*b*/
+.banner {
+  margin-top: 80px;
+  height: 392px;
+}
+
+/*c*/
+.card {
+  position: relative;
+}
+.carroussel-card {
+  height: inherit;
+  width: auto;
+  position: relative;
+}
+.c-image {
+  position: absolute;
+  height: auto;
+  width: auto;
+  border-radius: 5px;
+}
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.7s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+/*i*/
+.icon {
+  font-size: 8px;
+  margin-left: 8px;
+  background: #c7c7cd;
+  border-radius: 100%;
+  color: #c7c7cd;
+}
+.infosubtitle {
+  font-size: 12px;
+  font-weight: 400 !important;
+}
+.infotitle {
+  font-size: 14px;
+  font-weight: 500 !important;
+}
+
+/*m*/
+.menu {
+  padding-bottom: 0px !important;
+}
+.menu-item-content {
+  margin-bottom: 16px;
+  padding-bottom: 0px;
+  padding-top: 0px;
+}
+
+.menu-item:hover {
+  color: #f68b1e;
+}
+.menu-item {
+  font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Helvetica Neue", Arial, sans-serif;
+  font-weight: 400;
+  padding: 0px;
+  margin: 0px;
+  line-height: normal;
+  font-size: 12px !important;
+  color: #282828;
+}
+/*v*/
+.v-application {
+  background-color: #f5f5f5;
+}
+</style>
+
+
